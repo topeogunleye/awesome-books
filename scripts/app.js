fe24ab83766662f3aba793137d76e37ed80f2670
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-11-24 16:47:03 +0100

    Add Navbar and script to highlight active nav

--- scripts/app.js
+++ scripts/app.js
@@ -61,7 +61,7 @@ class UI {
   }
 
   static addBookToList(book) {
-    const bookList = document.getElementById('booklist');
+    const bookList = document.getElementById('books-container');
 
     const div = document.createElement('div');
 
@@ -125,4 +125,54 @@ document.getElementById('booklist').addEventListener('click', (e) => {
   Store.removeBook(e.target.previousElementSibling.textContent);
 });
 
-// Generate Random JavaScript ID
+// window.addEventListener('DOMContentLoaded', () => {
+// https://codepen.io/ljc-dev/embed/GRoLWxj?height=600&default-tab=js,result&embed-version=2
+// https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
+const navbar = document.querySelector('.navbar'); // list
+const ham = document.querySelector('.ham'); // button
+const menuLinks = document.querySelectorAll('.menuLink');
+
+function toggleHamburger() {
+  navbar.classList.toggle('showNav');
+  ham.classList.toggle('showClose');
+}
+
+// toggles hamburger menu in and out when clicking on the hamburger
+ham.addEventListener('click', toggleHamburger);
+
+// toggle when clicking on links
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', toggleHamburger);
+});
+
+
+// Add Highlight to Active Nav Link
+// https://dev.to/areeburrub/change-nav-link-s-style-as-you-scroll-4p62
+const sections = document.querySelectorAll('section');
+console.log(sections)
+
+const navLi = document.querySelectorAll('.navbar ul li a');
+
+window.onscroll = () => {
+  let current = '';
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    // console.log(sectionTop)
+
+    // eslint-disable-next-line no-restricted-globals
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute('id');
+      console.log(current);
+    }
+  });
+
+  navLi.forEach((li) => {
+    // console.log(li);
+    li.classList.remove('active');
+
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
+    }
+  });
+};
